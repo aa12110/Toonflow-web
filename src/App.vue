c45b03d84cd82eb18202ecefc48b58618c7fd2df
ACT丶流星雨 <1340145680@qq.com>
2026-02-26 18:41:21 +0800

    完善请求地址配置，新增退出设置

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,39 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted } from "vue";
+import { useRoute } from "vue-router";
 import zhCN from "ant-design-vue/es/locale/zh_CN";
 import UpdateDialog from "@/components/update.vue";
+import { initTheme } from "@/utils/theme";
+import settingStore from "@/stores/setting";
+import { storeToRefs } from "pinia";
+
+const route = useRoute();
+const store = settingStore();
+const { baseUrl, wsBaseUrl } = storeToRefs(store);
+
+// 从 URL query 参数设置请求地址
+const initFromQuery = () => {
+  const query = route.query;
+
+  // 支持通过 ?baseUrl=xxx 设置请求地址
+  if (query.baseUrl && typeof query.baseUrl === "string") {
+    baseUrl.value = query.baseUrl;
+  }
+
+  // 支持通过 ?wsBaseUrl=xxx 设置 WebSocket 地址
+  if (query.wsBaseUrl && typeof query.wsBaseUrl === "string") {
+    wsBaseUrl.value = query.wsBaseUrl;
+  }
+};
+
+// 初始化主题
+onMounted(() => {
+  initTheme();
+  initFromQuery();
+});
+
 const theme = {
   token: {
     colorPrimary: "#9810fa",
@@ -19,7 +50,7 @@ const theme = {
 };
 
 import { merge } from "lodash-es";
-import enConfig from "tdesign-vue-next/es/locale/en_US";
+import zhConfig from "tdesign-vue-next/es/locale/zh_CN";
 
 import { type GlobalConfigProvider } from "tdesign-vue-next";
 const empty: GlobalConfigProvider = {};
@@ -28,7 +59,7 @@ const customConfig: GlobalConfigProvider = {
   table: {},
   pagination: {},
 };
-const globalConfig: GlobalConfigProvider = merge(empty, enConfig, customConfig);
+const globalConfig: GlobalConfigProvider = merge(empty, zhConfig, customConfig);
 
 // document.documentElement.setAttribute('theme-mode', 'dark');
 // document.documentElement.setAttribute('theme-mode', 'light');
